a6e63e530535790ea28f08d5ecf760961617ca9e
Adriel Teles <adrielteles@outlook.com>
2023-10-28 17:58:06 -0300

    initial project

---
+++ MeowTalk/MeowTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowTalkApp.swift
+//  MeowTalk
+//
+//  Created by Adriel Teles on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MeowTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
